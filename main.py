9435b8a9a205de7c7cad67a2160aa6a4b0be8569
sen-ol <senolevsen@gmail.com>
2022-12-30 20:58:48 +0300

    avatarlar eklendi

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import datetime
+import os
 from datetime import date
 
 import pyrebase
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty, StringProperty, BooleanProperty
+from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
 from kivymd.uix.behaviors import CommonElevationBehavior
@@ -19,12 +22,19 @@ class HadiKart(CommonElevationBehavior, MDFloatLayout):
     isim = StringProperty()
     aciklama = StringProperty()
     eklenen_tarih = StringProperty()
+    avatar = StringProperty()
+
+
+class AvatarButonu(ButtonBehavior, Image):
+    source = StringProperty()
 
 
 class Hadi(MDApp):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.secilen_avatar = None
+
         firebaseConfig = {
             "apiKey": "AIzaSyCmDwDRshOShV-jLIrios1T8ev6344MKQs",
             "authDomain": "hadi-ac6cb.firebaseapp.com",
@@ -71,26 +81,57 @@ class Hadi(MDApp):
             for single_data in veri_seti.each():
                 veri_dict = single_data.val()
                 if veri_dict["Hadi_ismi"] != "":
+                    avatar_source=f"ikonlar/{veri_dict['Avatar']}.png"
                     baslangic_hadi_listesi = veri_dict["Hadi_ismi"]
                     ekran_yoneticisi.get_screen("Hadilerim").hadi_listesi.add_widget(
                         HadiKart(isim=veri_dict["Hadi_ismi"],
                                  aciklama=veri_dict["Hadi_Bilgi"],
-                                 eklenen_tarih=veri_dict["Eklenen_Zaman"]))
+                                 eklenen_tarih=veri_dict["Eklenen_Zaman"],
+                                 avatar=avatar_source))
+
+        folder = 'ikonlar'
+        avatar_listesi = os.listdir(folder)
+
+        for file in avatar_listesi:
+
+            # print(file)
+
+            avatar = AvatarButonu(source=f"ikonlar/{file}")
+
+            ekran_yoneticisi.get_screen("Hadi_Ekle").avatar_listesi.add_widget(avatar)
+
+
+
+    def avatar_secildi(self, dosya_ismi):
+
+        secilen_dosya = os.path.basename(dosya_ismi.source)
+
+        self.secilen_avatar = os.path.splitext(secilen_dosya)[0]
+
+        ekran_yoneticisi.get_screen("Hadi_Ekle").avatar_secildi_popup.text = f"{self.secilen_avatar} seçildi"
+
+
+
+
+        """try:
+            self.ids.avatar.source = dosya_ismi[0]
+            print(dosya_ismi[0])
+        except:
+            pass"""
 
     def on_complete(self, checkbox, value, isim, aciklama, bar):
         if value:
             isim.text = isim = f"[s]{isim.text}[/s]"
             aciklama.text = aciklama = f"[s]{aciklama.text}[/s]"
             # eklenen_tarih.text = eklenen_tarih = f"[s]{eklenen_tarih.text}[/s]"
-            bar.md_bg_color = 0, 179/255, 0, 1
+            bar.md_bg_color = 0, 179 / 255, 0, 1
         else:
             remove = ["[s]", "[/s]"]
             for i in remove:
                 isim.text = isim.text.replace(i, "")
                 aciklama.text = aciklama.text.replace(i, "")
                 # eklenen_tarih.text = eklenen_tarih.text.replace(i, "")
-                bar.md_bg_color = 1, 170/255, 23/255, 1
-
+                bar.md_bg_color = 1, 170 / 255, 23 / 255, 1
 
     def hadi_ekle(self, hadi_girilen_isim, hadi_girilen_bilgi):
         hadi_ismi = hadi_girilen_isim
@@ -98,12 +139,17 @@ class Hadi(MDApp):
         yapilma_durumu = False
         eklenen_zaman = self.tarih
 
-        veri = {"Hadi_ismi": hadi_ismi, "Hadi_Bilgi": hadi_bilgi, "Eklenen_Zaman": eklenen_zaman, "Durum": yapilma_durumu}
+        veri = {"Hadi_ismi": hadi_ismi, "Hadi_Bilgi": hadi_bilgi, "Eklenen_Zaman": eklenen_zaman,
+                "Durum": yapilma_durumu, "Avatar": self.secilen_avatar}
 
         self.veriler.child(hadi_ismi).set(veri)
 
         ekran_yoneticisi.get_screen("Hadi_Ekle").hadi_girilen_bilgi.text = ""
         ekran_yoneticisi.get_screen("Hadi_Ekle").hadi_girilen_isim.text = ""
+
+    """def avatar_secildi(self):
+        ekran_yoneticisi.get_screen("Hadi_Ekle").avatar_secildi_popup.text = "Avatar seçildi"
+"""
 
     def show_time_picker(self):
         time_dialog = MDTimePicker()
